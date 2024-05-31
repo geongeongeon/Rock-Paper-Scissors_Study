a132290dd6c9cfdf41a90119f4aa5122ec5fd4c8
geongeongeon <leegeonhee0204@naver.com>
2024-05-31 18:10:05 +0900

    회원 정보 수정 구현

--- src/main/java/com/gh/study/App.java
+++ src/main/java/com/gh/study/App.java
@@ -9,11 +9,26 @@ import static com.gh.study.container.Container.random;
 import static com.gh.study.container.Container.scanner;
 
 public class App {
-    Map<Integer, UserModel> userModelMap = new HashMap<>();
-    boolean isLogin = false; //로그인 상태
-    UserModel loginUser = null; //로그인한 유저 정보
+    static Map<Integer, UserModel> userModelMap;
+    static boolean isLogin;
+    static UserModel loginUser;
 
-    int userNo = 0;
+    int userNo;
+
+    public App() {
+        userModelMap = new HashMap<>();
+
+        //테스트 회원 데이터 생성
+        for(int i = 1; i <= 9; i ++) {
+            UserModel userModel = new UserModel(++userNo, "testId" + i, "testPw" + i, "testNickname" + i);
+            userModelMap.put(userNo, userModel);
+        }
+
+        isLogin = true; //로그인 상태
+        loginUser = userModelMap.get(4); //로그인한 유저 정보
+
+        userNo = 0;
+    }
 
     public void run() {
         while(true) {
@@ -136,6 +151,7 @@ public class App {
                 System.out.print("NICKNAME) ");
                 userNickname = scanner.nextLine();
 
+
                 boolean hasNickname = false;
 
                 for(Integer no : userModelMap.keySet()) {
@@ -157,12 +173,6 @@ public class App {
             userModelMap.put(userNo, userModel);
 
             System.out.println("===== success! =====");
-
-            //userList
-            for(Integer no : userModelMap.keySet()) {
-                UserModel user = userModelMap.get(no);
-                System.out.printf("%d | %s | %s | %s\n", user.getUserNo(), user.getUserId(), user.getUserPw(), user.getUserNickname());
-            }
         } else if(cmd.equals("/usr/login")) {
             System.out.println("===== login! =====");
 
@@ -208,7 +218,6 @@ public class App {
                             System.out.println("===== success! =====");
                             isLogin = true;
                             loginUser = userModelMap.get(key_loginUser);
-                            System.out.println(loginUser);
                             break;
                         } else {
                             System.out.println("===== failed! =====");
@@ -219,15 +228,108 @@ public class App {
                     System.out.println("===== non-existent ID! ===== ");
                 }
             }
-        } else if(cmd.equals("/usr/modify")) {
-            System.out.println("===== modify! =====");
+        } else if(cmd.equals("/usr/whoami")) {
+            System.out.println(loginUser);
+        } else if(cmd.split("\\?", 2)[0].equals("/usr/modify")) {
+            if(!isLogin) {
+                System.out.println("===== login first! =====");
+            } else {
+                String cmdSplit = cmd.split("\\?")[1];
+                String[] splitAnd = cmdSplit.split("&", 2);
+
+                Map<String, String> checkIdPwMap = new HashMap<>();
+
+                for(String param : splitAnd) {
+                    String[] splitEqual = param.split("=", 2);
+                    String key_checkIdPwMap = splitEqual[0];
+                    String value_checkIdPwMap = splitEqual[1];
+                    checkIdPwMap.put(key_checkIdPwMap, value_checkIdPwMap);
+                }
+
+                boolean hasKey_id = false;
+                boolean hasKey_pw = false;
+
+                for(String key : checkIdPwMap.keySet()) {
+                    if(key.equals("id")) {
+                        hasKey_id = true;
+                    } else if (key.equals("pw")) {
+                        hasKey_pw = true;
+                    }
+                }
+                if(hasKey_id && hasKey_pw) {
+                    System.out.println("===== modify! =====");
+
+                    if(checkIdPwMap.get("id").equals(loginUser.getUserId()) && checkIdPwMap.get("pw").equals(loginUser.getUserPw())) {
+                        while(true) {
+                            System.out.print("change) ");
+                            String whatChange = scanner.nextLine();
+
+                            if(whatChange.equals("id")) {
+                                System.out.println("===== can't change ID! =====");
+                            } else if(whatChange.equals("pw")) {
+                                while(true) {
+                                    System.out.print("new PASSWORD) ");
+                                    String newPassword = scanner.nextLine();
+
+                                    System.out.print("check new PASSWORD) ");
+                                    String check_newPassword = scanner.nextLine();
+
+                                    if(newPassword.equals(check_newPassword)) {
+                                        System.out.println("===== success! =====");
+                                        loginUser.setUserPw(newPassword);
+                                        break;
+                                    } else {
+                                        System.out.println("===== PASSWORD does not match! =====");
+                                    }
+                                }
+                            } else if(whatChange.equals("nickname")) {
+                                while(true) {
+                                    System.out.print("new NICKNAME) ");
+                                    String newNickname = scanner.nextLine();
+
+                                    boolean hasNewNickname = false;
+
+                                    for(Integer no : userModelMap.keySet()) {
+                                        UserModel user = userModelMap.get(no);
+                                        if(newNickname.equals(user.getUserNickname())) {
+                                            hasNewNickname = true;
+                                        }
+                                    }
+
+                                    if(hasNewNickname) {
+                                        System.out.println("===== NICKNAME does already existent! =====");
+                                    } else {
+                                        System.out.println("===== success! =====");
+                                        loginUser.setUserNickname(newNickname);
+                                        break;
+                                    }
+                                }
+                            } else if(whatChange.equals("/home")) {
+                                System.out.println("===== home! =====");
+                                return;
+                            } else {
+                                System.out.println("===== unknown command! =====");
+                            }
+                        }
+                    } else {
+                        System.out.println("===== ID or PASSWORD is not matched! ======");
+                    }
+                } else {
+                    System.out.println("===== unknown command! =====");
+                }
+            }
         } else if(cmd.equals("/usr/logout")) {
             if(!isLogin) {
                 System.out.println("===== login first! =====");
             } else {
-                System.out.println("===== logout! =====");
+                System.out.println("===== success! =====");
                 isLogin = false;
                 loginUser = null;
+            }
+        } else if(cmd.equals("/usr/list")) {
+            for(Integer no : userModelMap.keySet()) {
+                UserModel user = userModelMap.get(no);
+                System.out.printf("%d | %s | %s | %s\n", user.getUserNo(), user.getUserId(), user.getUserPw(), user.getUserNickname());
             }
         } else if(cmd.equals("/app/exit")) {
             System.out.print("===== exit! =====");
